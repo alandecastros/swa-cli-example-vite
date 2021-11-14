44e1bde971dd1bc197fb42137f2688a3d126fcfa
Kazuyuki Miyake <k-miyake@zengeeks.io>
2021-11-14 11:34:32 +0900

    chore: format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,19 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: [
-        'plugin:vue/vue3-recommended',
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'prettier',
-    ],
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-    },
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
 }
